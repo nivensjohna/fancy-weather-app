a691bf0b61ae7e31b20d9a2842f39117b7eeffbb
nivensjohna <133821608+nivensjohna@users.noreply.github.com>
2023-05-24 18:26:02 -0400

    added js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
-let apiKey = "d92691863b38409a95fa3b4d653fc425";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
 
-console.log(apiUrl);
+let apiKey = "d92691863b38409a95fa3b4d653fc425";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(Url).then(displayTemperature);
