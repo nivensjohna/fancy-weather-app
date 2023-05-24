17168d6076e25eca6997963c9e2701718d94b325
nivensjohna <133821608+nivensjohna@users.noreply.github.com>
2023-05-24 18:47:05 -0400

    more js added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#speed");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = respnse.data.main.humidity;
+  speedElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "d92691863b38409a95fa3b4d653fc425";
