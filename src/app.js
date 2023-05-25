1a4afe8f191ebc65ce5a21048bae7fa0c01fcdb9
nivensjohna <133821608+nivensjohna@users.noreply.github.com>
2023-05-25 18:29:53 -0400

    Update of all 3 files

--- src/app.js
+++ src/app.js
@@ -39,9 +39,10 @@ function displayTemperature(response) {
   speedElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
-    "i class",
+    "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let city = "New York";
