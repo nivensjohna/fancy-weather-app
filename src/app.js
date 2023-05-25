efc8ee25cb1e423004b08417d35f8d53c19b3cfa
nivensjohna <133821608+nivensjohna@users.noreply.github.com>
2023-05-25 18:17:00 -0400

    iconElement upgraded

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-    console.log(response.data)
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -31,15 +31,17 @@ function displayTemperature(response) {
   let speedElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
-  
-  
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   speedElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute() = `http://openweathermap.org/img/wn/04@2x.png`
+  iconElement.setAttribute(
+    "i class",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 let city = "New York";
