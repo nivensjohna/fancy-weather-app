7aa88e098375bad1ca73b92274954c6f8df39337
nivensjohna <133821608+nivensjohna@users.noreply.github.com>
2023-05-25 18:33:07 -0400

    Testing cities

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let city = "Denver";
+let city = "New York";
 let apiKey = "d92691863b38409a95fa3b4d653fc425";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
