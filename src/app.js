2c04b0d6b6c653e587ec8d66cfac75eb96462939
nivensjohna <133821608+nivensjohna@users.noreply.github.com>
2023-05-30 18:00:52 -0400

    Javascript added

--- src/app.js
+++ src/app.js
@@ -22,6 +22,33 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast () {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row"> ;
+   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+   days.foreach(function(day) {
+    forecasteHTML = 
+    forecastHTML + 
+     `
+    <div class="col-2">
+    <div class="weather-forecast-date">${day}</div>   
+    <img src="http://openweathermap.org/img/wn/50d@2x.png"  
+    alt="" 
+    width="42"
+    />
+   <div class="weather-forecast-temperatures">
+   <span class="weather-forecast-temperature-max"> 18° </span>
+   <span class="weather-forecast-temperature-min"> 12° </span>
+              </div>
+            </div>
+          </div>
+        `;
+         });
+
+        forecastHTML = forecastHTML + `</div>`;
+        forecastElement.innerHTML = forecastHTML
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -31,6 +58,8 @@ function displayTemperature(response) {
   let speedElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
+
+  
 
   celsiusTemperature = response.data.main.temp;
 
@@ -76,6 +105,8 @@ function displayCelsiusTemperature(event) {
 
 let celsiusTemperature = null;
 
+let displayForecast();
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
@@ -86,3 +117,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+
+let displayForecast();
