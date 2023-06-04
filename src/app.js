d81520f35b0ba2a00b89985009c10e3765999b8f
nivensjohna <133821608+nivensjohna@users.noreply.github.com>
2023-06-03 21:59:33 -0400

    fahrenheit classList removed

--- src/app.js
+++ src/app.js
@@ -113,7 +113,6 @@ function handleSubmit(event) {
 function displayCelsiusTemperature(event) {
   event.preventDefault();
   celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
